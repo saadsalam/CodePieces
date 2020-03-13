82d459622902827337069a383def4281f76cac11
saadsalam <saadofficial@gmail.com>
2020-03-13 13:44:48 -0400

    Update Service.Private.cs

--- App/Service.Private.cs
+++ App/Service.Private.cs
@@ -1,1 +1,12 @@
 
+private DataTable GetUnpaidExpenseList(string pDriver)
+		{
+			// ErrorLog
+			WebServiceException.WriteError("getUnpaidExpenseList inside the function()");
+
+			var dtReturn = new DataTable("UnpaidExpenses");
+
+			dtReturn.Columns.Add("ExpenseID", typeof(string));
+			dtReturn.Columns.Add("DriverID", typeof(string));
+			dtReturn.Columns.Add("ItemDate", typeof(string));
+}
